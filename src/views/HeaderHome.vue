ef3529c76c4738e07ff869ff87de45f3febe2dbc
knagara <nagara.keita@gmail.com>
2021-03-17 14:55:19 +0900

    use store

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -3,8 +3,19 @@
         <h2>Home</h2>
       <router-link to="/" active-class="link--active" exact>Home</router-link>
       <router-link to="/users/1" active-class="link--active" exact>Users</router-link>
+      <button @click="increment">+1</button>
     </nav>
 </template>
+
+<script>
+export default {
+  methods:{
+    increment(){
+      this.$store.state.count++;
+    }
+  }
+}
+</script>
 
 <style scoped>
   .link--active {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,21 @@
     <div>
         <h1>Home</h1>
         <button @click="toUsers">to users</button>
+        <p>{{ count }}</p>
+        <p>{{ doubleCount }}</p>
+        <p>{{ tripleCount }}</p>
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
+    computed: {
+        count(){
+            return this.$store.state.count;
+        },
+        ...mapGetters(["doubleCount", "tripleCount"]),
+    },
     methods:{
         toUsers(){
             this.$router.push({
